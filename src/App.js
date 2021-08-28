c1aff53c8745f055a8c3ec1b38877f66ba16bd65
dwiklich <d.wiklich@gmail.com>
2021-08-28 13:24:07 -0300

    Updated file App.js

--- src/App.js
+++ src/App.js
@@ -1,106 +1,58 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 import { Card } from "./components/Card";
-
-const requestPages = "https://rickandmortyapi.com/graphql";
+import { loadCharacter } from "./utils/loadCharacters";
+import { graphQLRequestTotalPages } from "./utils/graphQLRequestTotalPages";
 
 class App extends Component {
   state = {
     characters: [],
   };
 
-  componentDidMount() {
-    this.loadCharacters();
+  async componentDidMount() {
+    await this.loadCharacters();
   }
 
-  async graphQLRequestPages() {
-    const query = `query {
-      characters(page: 1) {
-        info {
-          pages
-        }
-      }
-    }`;
+  // async graphQLRequestPagesCHaractersAll() {
+  //   const query = `query {
+  //     characters(page: 1) {
+  //       results {
+  //         name
+  //       }
+  //       }
+  //       location(id: 1) {
+  //         id
+  //       }
+  //       episodesByIds(ids: [1, 2]) {
+  //         id
+  //       }
+  //     }
+  //   }`;
 
-    const dataPromise = fetch(requestPages, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query,
-      }),
-    });
+  //   const dataPromise = fetch(requestPages, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       query,
+  //     }),
+  //   });
 
-    const [dataConvert] = await Promise.all([dataPromise]);
-    const dataJson = await dataConvert.json();
-    return dataJson.data.characters.info.pages;
-  }
-
-  async graphQLRequestPagesCHaractersAll() {
-    const query = `query {
-      characters(page: 1) {
-        results {
-          name
-        }
-        }
-        location(id: 1) {
-          id
-        }
-        episodesByIds(ids: [1, 2]) {
-          id
-        }
-      }
-    }`;
-
-    const dataPromise = fetch(requestPages, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query,
-      }),
-    });
-
-    const [dataConvert] = await Promise.all([dataPromise]);
-    const dataJson = await dataConvert.json();
-    const pagesCharacteres = await dataJson.data.characters.info.pages;
-    return pagesCharacteres;
-  }
+  //   const [dataConvert] = await Promise.all([dataPromise]);
+  //   const dataJson = await dataConvert.json();
+  //   const pagesCharacteres = await dataJson.data.characters.info.pages;
+  //   return pagesCharacteres;
+  // }
 
   loadCharacters = async () => {
-    let characters = [];
-    let pageNumber = 1;
-    const pagesCharacteres = await this.graphQLRequestPages();
-    console.log(pagesCharacteres);
+    const totalPagesCharacteres = await graphQLRequestTotalPages();
 
-    const charactersUrlRequest =
-      "https://rickandmortyapi.com/api/character/?page=";
-
-    for (let i = 1; i <= pagesCharacteres; i++) {
-      const charactersResponse = fetch(charactersUrlRequest + i);
-
-      const [charactersConvert] = await Promise.all([charactersResponse]);
-
-      let charactersAll = await charactersConvert.json();
-
-      characters.push(...charactersAll.results);
-    }
-
+    const characters = await loadCharacter(totalPagesCharacteres);
     this.setState({
       characters: [...characters],
     });
-
-    // }
-
-    // zip de array
-    // const postsAndPhotos = postsJson.map((post, index) => {
-    //   return { ...post, cover: photosJson[index].url };
-    // });
   };
 
   render() {
